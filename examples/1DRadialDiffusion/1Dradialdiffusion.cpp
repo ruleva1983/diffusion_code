7c5b7420eea22ad96cb270f1188ce3e8d8f468e9
Ruggero Vasile <ruggero@mag37.gfz-potsdam.de>
2018-09-10 21:02:16 +0200

    Testing radial diffusion

--- examples/1DRadialDiffusion/1Dradialdiffusion.cpp
+++ examples/1DRadialDiffusion/1Dradialdiffusion.cpp
@@ -11,40 +11,43 @@
 
 
 
-const int N = 20;
-const float xL = 1, xR = 6;
-const float dt = 0.01;
+const int N = 50;
+const float xL = 1, xR = 7;
+const float dt = 0.02;
 const float t0 = 0;
-const float tF = 5.0;
+const float tF = 100.0;
 const float upperboundary = 7.2;
-const float lowerboundary = 1.0;
-const float tau=10.0;
+const float lowerboundary = 0.0;
+const float tau=1.0;
 
 using FuncType = std::function<float(float, float)>;
 
 
 float InitialState(float x){
-    if (x < 2)
-        return 1,0;
-    else
+    /*if (x < 2)
         return 1.0;
+    else
+        return 1.0;*/
+
+    return std::exp(-(x-3)*(x-3)/(2*0.1));
 }
 
 float kp(float t){
-    return 2.0;
+    return 6.0;
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
 
     FuncType a = [](float x, float t){return std::pow(x, 10)*std::pow(10,0.506*kp(t)-9.325);};
     FuncType b = [](float x, float t){return 8.0*std::pow(x, 9)*std::pow(10,0.506*kp(t)-9.325);};
     FuncType c = [tau](float x, float t){return -1/tau;};
     FuncType d = [](float x, float t){return 0.0;};
-    Functional<FuncType> A(a);
+    FuncType diff = [](float x, float t){return 1.0;};
+    Functional<FuncType> A(diff);
     Functional<FuncType> B(d);
-    Functional<FuncType> C(d);
+    Functional<FuncType> C(c);
     Functional<FuncType> D(d);
     
     std::cout << A(1,1) << " " << A(2, 1);
@@ -59,8 +62,8 @@ int main()
     Grid1D<> grid(N, xL, xR, Xin);
 
     Boundary1D<> boundary(lowerboundary, upperboundary);
-    ImplicitScheme1D scheme(grid.get_deltax(), A, B, C, D);
-    solve1D<ImplicitScheme1D, Grid1D<>, Boundary1D<>,
+    CrankNicholson scheme(grid.get_deltax(), A, B, C, D, 0.5);
+    solve1D<CrankNicholson, Grid1D<>, Boundary1D<>,
             std::vector<std::vector<float>>>(grid, scheme, boundary, Obs, t0, tF, dt);
 
     std::ofstream file("1DRadialDiffusion.dat");
--- include/integrate.hpp
+++ include/integrate.hpp
@@ -83,19 +83,20 @@ public:
     ImplicitScheme1D(float deltax, Coeff a, Coeff b, Coeff c, Coeff d ) : Scheme1D(), dx(deltax), A(a), B(b), C(c), D(d)
     {
     }
-    
+
+    //TODO Look at the sign differencecs between the diffusion and advection parts
     void evaluate_A(const Eigen::VectorXf& X, float t, float dt){
         MatA = Eigen::MatrixXf::Zero (X.size(), X.size());
         MatA(0,0) = 1 + 2*dt/(dx*dx)*A(X(0),t+dt) - dt*C(X(0), t+dt);
-        MatA(0,1) = -dt/(dx*dx)*A(X(0),t+dt) + dt*B(X(0),t+dt);
+        MatA(0,1) = -dt/(dx*dx)*A(X(0),t+dt) - dt*B(X(0),t+dt)/(2*dx);
         for (int i = 1 ; i < X.size()-1 ; ++i )
         {
             MatA(i,i) = 1 + 2*dt/(dx*dx)*A(X(i),t+dt) - dt*C(X(i), t+dt);
-            MatA(i,i-1) = -dt/(dx*dx)*A(X(i),t+dt) - dt*B(X(i),t+dt);
-            MatA(i,i+1) = -dt/(dx*dx)*A(X(i),t+dt) + dt*B(X(i),t+dt);
+            MatA(i,i-1) = -dt/(dx*dx)*A(X(i),t+dt) + dt*B(X(i),t+dt)/(2*dx);
+            MatA(i,i+1) = -dt/(dx*dx)*A(X(i),t+dt) - dt*B(X(i),t+dt)/(2*dx);
         }
         MatA(X.size()-1, X.size()-1) = 1 + 2*dt/(dx*dx)*A(X(X.size()-1),t+dt) - dt*C(X(X.size()-1), t+dt);
-        MatA(X.size()-1, X.size()-2) = -dt/(dx*dx)*A(X(X.size()-1),t+dt) - dt*B(X(X.size()-1),t+dt);
+        MatA(X.size()-1, X.size()-2) = -dt/(dx*dx)*A(X(X.size()-1),t+dt) + dt*B(X(X.size()-1),t+dt)/(2*dx);
     }
 
     void evaluate_b(const Eigen::VectorXf& X, float t, float dt){
@@ -109,44 +110,46 @@ private:
     Coeff A, B, C, D;
 };
 
-/*
+
 class CrankNicholson: public Scheme1D{
 
 public:
-    CrankNicholson(float dx, float d) : Scheme1D(), deltax(dx), D(d)
+    CrankNicholson(float deltax, Coeff a, Coeff b, Coeff c, Coeff d , float mixture=0.5) : Scheme1D(),
+    dx(deltax), A(a), B(b), C(c), D(d), alpha(mixture)
     {
     }
-    
-    void evaluate_A(const Eigen::VectorXf& X, float dt){
-        float coeff = dt*D/(2*dt*dt);
-        A = Eigen::MatrixXf::Zero (X.size(), X.size());
-        A(0,0) = 1 + 2*coeff;
-        A(0,1) = -coeff;
+
+    void evaluate_A(const Eigen::VectorXf& X, float t, float dt){
+        MatA = Eigen::MatrixXf::Zero (X.size(), X.size());
+        MatA(0,0) = 1 + 2*dt*(1-alpha)/(dx*dx)*A(X(0),t+dt) - (1-alpha)*dt*C(X(0), t+dt);
+        MatA(0,1) = -dt*(1-alpha)/(dx*dx)*A(X(0),t+dt) - dt*B(X(0),t+dt)*(1-alpha)/(2*dx);
         for (int i = 1 ; i < X.size()-1 ; ++i )
         {
-            A(i,i) = 1 + 2*coeff;
-            A(i,i-1) = -coeff;
-            A(i,i+1) = -coeff;
+            MatA(i,i) = 1 + 2*dt*(1-alpha)/(dx*dx)*A(X(i),t+dt) - (1-alpha)*dt*C(X(i), t+dt);
+            MatA(i,i-1) = -dt*(1-alpha)/(dx*dx)*A(X(i),t+dt) + (1-alpha)*dt*B(X(i),t+dt)/(2*dx);
+            MatA(i,i+1) = -dt*(1-alpha)/(dx*dx)*A(X(i),t+dt) - (1-alpha)*dt*B(X(i),t+dt)/(2*dx);
         }
-        A(X.size()-1, X.size()-1) = 1+2*coeff;
-        A(X.size()-1, X.size()-2) = -coeff;
+        MatA(X.size()-1, X.size()-1) = 1 + 2*dt/(dx*dx)*A(X(X.size()-1),t+dt) - dt*C(X(X.size()-1), t+dt)*(1-alpha);
+        MatA(X.size()-1, X.size()-2) = -dt/(dx*dx)*A(X(X.size()-1),t+dt) + dt*B(X(X.size()-1),t+dt)*(1-alpha)/(2*dx);
     }
-    
-    //TODO Works with zero boundary conditions at the edges
-    void evaluate_b(const Eigen::VectorXf& X, float dt){
-        
-        float coeff = dt*D/(2*dt*dt);
-        b.resize(X.size());
-        for (int i = 1 ; i < X.size() - 1 ; ++i )
-            b(i) = coeff*(X(i-1) + X(i+1))+(1-2*coeff)*X(i);
 
+    void evaluate_b(const Eigen::VectorXf& X, float t, float dt){
+        Vecb.resize(X.size());
+        for (int i = 1 ; i < X.size() - 1 ; ++i ){
+            float coeff_i = 1 + alpha*dt*C(X(i),t) - 2*alpha*dt/(dx*dx)*A(X(i),t);
+            float coeff_ip1 = alpha*dt/(dx*dx)*A(X(i),t) + alpha*dt/(2*dx)*B(X(i),t);
+            float coeff_im1 = alpha*dt/(dx*dx)*A(X(i),t) - alpha*dt/(2*dx)*B(X(i),t);
+            float coeff = alpha*D(X(i),t) + (1-alpha)*D(X(i),t+dt);
+            Vecb(i) = coeff + coeff_i*X(i) + coeff_im1*X(i-1) + coeff_ip1*X(i+1);
+        }
     }
-    
+
 private:
-    float deltax;
-    float D;
+    float dx;
+    float alpha;
+    Coeff A, B, C, D;
 };
-*/
+
 
 
 #endif
